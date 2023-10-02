0221c5a99270c4c73bbc199d5cc76461bef9e0d9
Cristhian Almeida <crilordz@hotmail.com>
2023-10-02 19:57:07 -0400

    add:tipos_de_dados

---
+++ JavaScript/tiposdeDados/script.js
@@ -0,0 +1,16 @@
+// Declare uma variável contendo uma string
+var nome = "Cristhian";
+// Declare uma variável contendo um número dentro de uma string
+var numero = "2023";
+// Declare uma variável com a sua idade
+var idade = 26;
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = "Almeida";
+var nomeCompleto = `${nome}  ${sobrenome}`;
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome
+
+console.log(nome, numero, idade, nomeCompleto, frase, verificarTipoNome);
---
+++ JavaScript/variaveis/script.js
@@ -0,0 +1,8 @@
+var nome = 'cristhian';
+var idade = 26;
+var comidaFavorita;
+comidaFavorita = 'lasanha';
+
+var seila
+
+console.log(nome, idade, comidaFavorita, seila)
