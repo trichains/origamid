22edffc366f4420448e25960f58dd732d6318e50
Cristhian Almeida <crilordz@hotmail.com>
2023-10-09 16:09:26 -0400

    add functions

--- JavaScript/function/script.js
+++ JavaScript/function/script.js
@@ -17,30 +17,45 @@ console.log(lado);
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
 function nomeCompleto(nome, sobrenome) {
-  var nome
-  var sobrenome
-  return nome + sobrenome;
+  return `${nome} ${sobrenome}`;
 }
 
-var nomeCompleto = nomeCompleto(`${'Cristhian'} ${'Almeida'}`)
-console.log(nomeCompleto)
+var nomeCompletoString = nomeCompleto('Cristhian', 'Almeida')
+console.log(nomeCompletoString);
 // Crie uma função que verifica se um número é par
+function isEven(numero) {
+  return numero % 2 === 0;
+}
 
+var evenOrOdd = isEven(3);
+
+if (evenOrOdd) {
+  console.log("o numero é par")
+} else {
+  console.log("o numero é impar")
+}
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
+function tipoDeDado(dado) {
+  return typeof dado;
+}
+
+var dado = tipoDeDado(4);
+console.log(dado);
 
 // addEventListener é uma função nativa do JavaScript
 // o primeiro parâmetro é o evento que ocorre e o segundo o Callback
 // utilize essa função para mostrar no console o seu nome completo
-// quando o evento 'scroll' ocorrer.
-
+// quando o evento 'click' ocorrer.
+addEventListener('click', function () {
+  console.log('Cristhian');
+});
 // Corrija o erro abaixo
+var totalPaises = 193;
 function precisoVisitar(paisesVisitados) {
-  const totalPaises = 193;
   return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
 }
 function jaVisitei(paisesVisitados) {
-  const totalPaises = 193;
   return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
 }
 console.log(precisoVisitar(20));
