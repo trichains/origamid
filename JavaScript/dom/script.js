42a6414b31c8c2ffc056c18e4845a7389a8a7b96
Cristhian Almeida <crilordz@hotmail.com>
2023-10-16 09:14:51 -0400

    add: dom

---
+++ JavaScript/dom/script.js
@@ -0,0 +1,11 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const ativo = document.querySelector('.ativo');
+console.log(ativo);
+// Retorne a linguagem do navegador
+console.log(navigator.language);
+// Retorne a largura da janela
+console.log(innerWidth);
