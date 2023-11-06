524f2fde2bb0196cda83337f16a2dffd510e5eed
Cristhian Almeida <crilordz@hotmail.com>
2023-11-06 16:18:07 -0400

    organizando o estudo
    organizei as pastas e os arquivos dos estudos

--- JavaScript/constructorFuction1/script.js
+++
@@ -1,7 +0,0 @@
-function Refri(marca, preco) {
-  this.marca = marca;
-  this.preco = preco;
-}
-
-const coca = new Refri('Coca-Cola!', 15);
-const fanta = new Refri('Fanta!', 10);
--- JavaScript/variaveis/script.js
+++
@@ -1,8 +0,0 @@
-var nome = 'cristhian';
-var idade = 26;
-var comidaFavorita;
-comidaFavorita = 'lasanha';
-
-var seila
-
-console.log(nome, idade, comidaFavorita, seila)
