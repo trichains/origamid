4ac25f5ddbee6617e6daaba41fa537fa20856bd2
Cristhian Almeida <3correntes@gmail.com>
2023-11-16 15:57:40 -0400

    add exercicio fetch

--- JavaScript/06-javascript-assincrono/0603-fetch/script.js
+++ JavaScript/06-javascript-assincrono/0603-fetch/script.js
@@ -1,69 +1,63 @@
 // Utilizando a API https://viacep.com.br/ws/${CEP}/json/
 // crie um formulário onde o usuário pode digitar o cep
 // e o endereço completo é retornado ao clicar em buscar
-const inputCep = document.getElementById('cep');
-const btnCep = document.getElementById('btnCep');
-const resultadoCep = document.querySelector('.resultadoCep');
+const cepInput = document.getElementById('cep');
+const submitButton = document.getElementById('submit');
+const retornoElement = document.querySelector('.cepRetorno');
 
-btnCep.addEventListener('click', handleClick);
-
-function handleClick(event) {
+submitButton.addEventListener('click', (event) => {
   event.preventDefault();
-  const cep = inputCep.value;
-  buscaCep(cep);
-}
 
-function buscaCep(cep) {
-  fetch(`https://viacep.com.br/ws/${cep}/json/`)
-  .then(response => response.text())
-  .then(body => {
-    resultadoCep.innerText = body;
-  })
-}
+  const cep = cepInput.value;
+  const url = `https://viacep.com.br/ws/${cep}/json/`;
 
+  // Check if the cep input is empty
+  if (cep) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        // Update the retorno element with the retrieved data
+        retornoElement.innerHTML = `${data.logradouro}, ${data.bairro}, ${data.localidade}, ${data.uf}`;
+      });
+  } else {
+    alert('Por favor, digite um CEP');
+  }
+});
 
 // Utilizando a API https://blockchain.info/ticker
 // retorne no DOM o valor de compra da bitcoin and reais.
 // atualize este valor a cada 30s
+async function updateBitcoinPrice() {
+  const url = 'https://blockchain.info/ticker';
 
-const btcDisplay = document.querySelector('.btc');
-
-function fetchBtc() {
-  fetch('https://blockchain.info/ticker')
-  .then(response => response.json())
-  .then(btcJson => {
-    btcDisplay.innerText = ('R$ ' + btcJson.BRL.buy).replace('.', ',');
-  })
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const brlPrice = data.BRL.buy.toFixed(2);
+    document.querySelector('.bitcoin').innerHTML = `R$ ${brlPrice}`;
+  } catch (error) {
+    console.error(error);
+  }
 }
+setInterval(updateBitcoinPrice, 30000);
 
-// setInterval(fetchBtc, 1000 * 30);
-
-fetchBtc();
+updateBitcoinPrice();
 
 // Utilizando a API https://api.chucknorris.io/jokes/random
 // retorne uma piada randomica do chucknorris, toda vez que
 // clicar em próxima
-const btnProxima = document.querySelector('.proxima');
-const paragrafoPiada = document.querySelector('.piada');
 
-function puxarPiada() {
-  fetch('https://api.chucknorris.io/jokes/random')
-  .then(r => r.json())
-  .then(piada => {
-    paragrafoPiada.innerText = piada.value;
-  })
+const jokeButton = document.getElementById('get-joke');
+
+function getJoke() {
+  const apiUrl = 'https://api.chucknorris.io/jokes/random';
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector('.joke').innerHTML = data.value;
+    });
 }
 
-btnProxima.addEventListener('click', puxarPiada);
+jokeButton.addEventListener('click', getJoke);
 
-puxarPiada();
-
-function puxarPost() {
-  fetch('https://jsonplaceholder.typicode.com/posts/')
-  .then(r => r.json())
-  .then(r => {
-    console.log(r)
-  })
-}
-
-puxarPost();
+getJoke();
